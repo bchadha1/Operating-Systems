c5fda04d9a078f15974a2ee78e4f410795545062
Bhuvan Chadha <54380864+bchadha1@users.noreply.github.com>
2020-12-04 22:13:07 -0500

    Memory Mapping

---
+++ Assignment 3/memorymap.c
@@ -0,0 +1,38 @@
+#include <linux/mm.h>
+
+struct task_struct
+{
+	...
+	struct mm_struct*mm;
+	mm = 
+	...
+};
+
+struct mm_struct
+{
+	...
+	pgd_t*pgd;
+	...
+}
+
+struct pid*pid;
+struct task_struct*pid_struct;
+struct mm_struct*pid_mm_struct;
+
+pid=find_get_pid(process_ID);
+pid_struct=pid_task(pid, PIDTYPE_PID);
+pid_mm_struct=pid_struct->mm;
+pgd=pgd_offset(pid_struct->mm, address);
+
+for (vma= mm->mmap; vma; vma= vma->vm_next)
+{
+	for(vaddr= vma->vm_start; vaddr< vma->vm_end; vaddr++)
+	{
+		pgd= pgd_offset(mm, vaddr);
+		...
+		...
+	}
+}
+		
+		
+		
