293c8b3e354d97d4a54e74d975c22af674a9a9d3
Bhuvan Chadha <54380864+bchadha1@users.noreply.github.com>
2020-12-06 01:57:44 -0500

    Update memorymap.c

--- Assignment 3/memorymap.c
+++ Assignment 3/memorymap.c
@@ -6,28 +6,33 @@
 #include <linux/init.h>
 #include <linux/module.h>
 
-static struct page *walk_page_table(unsigned long addr)
+static struct page_vma_mapped_walk (unsigned long *pvmw)
 {
 
     struct pid*pid;
     struct task_struct*pid_struct;
-    struct mm_struct*pid_mm_struct;
+    struct mm_struct *mm = pvmw->vma->vm_mm;
+    struct page *page = pvmw->page;
     pgd_t *pgd;
-    pte_t *ptep, pte;
+    p4d_t *p4d;
     pud_t *pud;
-    pmd_t *pmd;
+    pmd_t pmde;
 
     struct page *page = NULL;
     struct mm_struct *mm = current->mm;
 
-    pgd = pgd_offset(mm, addr);
-    if (pgd_none(*pgd) || pgd_bad(*pgd))
-        goto out;
+    pgd = pgd_offset(mm, pvmw->address);
+    if (!pgd_present(*pgd))
+		return false;
     printk(KERN_NOTICE "Valid pgd");
+    
+    p4d = p4d_offset(pgd, pvmw->address);
+	if (!p4d_present(*p4d))
+		return false;
 
-    pud = pud_offset(pgd, addr);
-    if (pud_none(*pud) || pud_bad(*pud))
-        goto out;
+    pud = pud_offset(p4d, pvmw->address);
+	if (!pud_present(*pud))
+		return false;
     printk(KERN_NOTICE "Valid pud");
 
     pmd = pmd_offset(pud, addr);
@@ -35,12 +40,15 @@ static struct page *walk_page_table(unsigned long addr)
         goto out;
     printk(KERN_NOTICE "Valid pmd");
 
-    ptep= pte_offset_map(pmd, addr);
+    ptep = pte_offset_map(pmd, addr);
     if (!ptep)
         goto out;
     pte = *ptep;
+    
+    if (!pmd_present(pmde)) {
+		return false;
 
-    page= pte_page(pte);
+    page = pte_page(pte);
     if (page)
     {
         printk(KERN_INFO "page frame struct is @ %p", page);
@@ -51,14 +59,11 @@ static struct page *walk_page_table(unsigned long addr)
     return page;
 }
 
-//struct pid*pid;
-//struct task_struct*pid_struct;
-//struct mm_struct*pid_mm_struct;
 
-//pid=find_get_pid(process_ID);
-//pid_struct=pid_task(pid, PIDTYPE_PID);
-//pid_mm_struct=pid_struct->mm;
-//pgd=pgd_offset(pid_struct->mm, address);
+pid=find_get_pid(process_ID);
+pid_struct=pid_task(pid, PIDTYPE_PID);
+pid_mm_struct=pid_struct->mm;
+pgd=pgd_offset(pid_struct->mm, address);
 
 //for (vma= mm->mmap; vma; vma= vma->vm_next)
 //{
