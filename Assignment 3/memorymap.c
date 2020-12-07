37102d134f64e30dd3e875d9cfed0e9ce21a0290
Bhuvan Chadha <54380864+bchadha1@users.noreply.github.com>
2020-12-06 22:00:33 -0500

    Update memorymap.c

--- Assignment 3/memorymap.c
+++ Assignment 3/memorymap.c
@@ -3,9 +3,9 @@
 #include <linux/hugetlb.h>
 #include <linux/swap.h>
 #include <linux/swapops.h>
+#include <linux/printk.h>
 
-#include "internal.h"
-static struct page_vma_mapped_walk (unsigned long *pvmw)
+bool page_vma_mapped_walk(struct page_vma_mapped_walk *pvmw)
 {
 
     unsigned long pfn;
@@ -21,12 +21,15 @@ static struct page_vma_mapped_walk (unsigned long *pvmw)
     pgd = pgd_offset(mm, pvmw->address);
     if (!pgd_present(*pgd))
 	return false;
+	
     p4d = p4d_offset(pgd, pvmw->address);
     if (!p4d_present(*p4d))
 	return false;
+	
     pud = pud_offset(p4d, pvmw->address);
     if (!pud_present(*pud))
 	return false;
+	
     pvmw->pmd = pmd_offset(pud, pvmw->address);
     pmde = READ_ONCE(*pvmw->pmd);
     if (!pmd_present(pmde))
@@ -35,10 +38,8 @@ static struct page_vma_mapped_walk (unsigned long *pvmw)
     pvmw->pte = pte_offset_map(pvmw->pmd, pvmw->address);
     if (!pte_present(*pvmw->pte))
 	return false;
+	
     pfn = pte_pfn(*pvmw->pte);
 
-    return page;
+    printk(KERN_INFO "Page frame number", page);
 }
-	
-		
-		
